9d8f373f4b4f7225c7db7e56f429e84ec0dcfe58
HarrisonBender <83611772+HarrisonBender@users.noreply.github.com>
2021-07-07 11:54:55 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-    return <h2>Studio Ghibli Home:
-         <br></br>
-        Please excuse the lack of styling in this first draft. An updated styling is to come!
-    </h2>;
+  return (
+
+    <picture>
+
+        <source
+        media="(orientation: landscape)"
+        srcSet="https://images.prismic.io/tokyotreat/243d7a4742342b7a1ce8db2cd21d757ebd215a68_studio_ghibli_characters_by_ficklestix-d846q1x.jpg?auto=compress,format"></source>
+
+        <source
+        media="(orientation: portrait)"
+        srcSet="https://i.pinimg.com/originals/b3/b5/89/b3b589f91bb77ae59ef3a5116caf3027.jpg"></source>
+    </picture>
+
+
+   
+  );
 }
